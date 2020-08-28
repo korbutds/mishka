7d9f7d08986a9fd203bd9e2f4550a9798f38c9d3
korbutds <sys@hohou.ru>
2020-08-28 21:20:14 +0300

    Выполнен ПП мобильной index. Добавлено поведение модального окна

--- source/js/toggle.js
+++ source/js/toggle.js
@@ -1,5 +1,9 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var orderBtn = document.querySelector('.js-order');
+var cartModal = document.querySelector('.cart-modal');
+var overlay = document.querySelector('.overlay');
+var cartModalOrder = document.querySelector('cart-modal__button');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -11,4 +15,26 @@ navToggle.addEventListener('click', function() {
     navMain.classList.add('main-nav--close');
     navMain.classList.remove('main-nav--open');
   }
+});
+
+orderBtn.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  if (cartModal.classList.contains('cart-modal--close')) {
+    cartModal.classList.remove('cart-modal--close')
+    overlay.classList.add('overlay--active')
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    overlay.classList.remove('overlay--active')
+    cartModal.classList.add('cart-modal--close');
+  };
+});
+
+cartModalOrder.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  overlay.classList.remove('overlay--active')
+  cartModal.classList.add('cart-modal--close');
 });
