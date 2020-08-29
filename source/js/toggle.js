2c2bd9b5629a2f3faa725d5d6bc7b14e0005b71d
korbutds <sys@hohou.ru>
2020-08-29 21:17:42 +0300

    Сделан ПП index

--- source/js/toggle.js
+++ source/js/toggle.js
@@ -3,7 +3,11 @@ var navToggle = document.querySelector('.main-nav__toggle');
 var orderBtn = document.querySelector('.js-order');
 var cartModal = document.querySelector('.cart-modal');
 var overlay = document.querySelector('.overlay');
-var cartModalOrder = document.querySelector('cart-modal__button');
+
+var cartModalClose = function() {
+  overlay.classList.remove('overlay--active');
+  cartModal.classList.add('cart-modal--close');
+};
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -22,19 +26,26 @@ orderBtn.addEventListener('click', function(evt) {
   if (cartModal.classList.contains('cart-modal--close')) {
     cartModal.classList.remove('cart-modal--close')
     overlay.classList.add('overlay--active')
+
+
+  }
+});
+
+document.addEventListener('click', function(evt) {
+  var isClickInside = cartModal.contains(evt.target);
+  if (isClickInside) {
+    console.log('click inside')
+    cartModalClose();
+  } else {
+    console.log('click outside')
   }
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    overlay.classList.remove('overlay--active')
-    cartModal.classList.add('cart-modal--close');
+    cartModalClose();
+    console.log('close')
   };
 });
 
-cartModalOrder.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  overlay.classList.remove('overlay--active')
-  cartModal.classList.add('cart-modal--close');
-});
